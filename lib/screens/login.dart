dda99589ae0e2ffe8b97fa1486660931c3a48239
SriGiriT <srigirit369@gmail.com>
2023-05-03 08:59:08 +0530

    firebase

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:muscle_cramp/components/RoundedButton.dart';
 import 'package:muscle_cramp/components/RoundedInputFeild.dart';
 import 'package:muscle_cramp/screens/reading_screen.dart';
@@ -21,37 +20,6 @@ class _LoginPageState extends State<LoginPage> {
     setState(() {
       _isNewUser = value;
     });
-  }
-
-  void _submitForm() async {
-    try {
-      if (_isNewUser) {
-        await FirebaseAuth.instance.createUserWithEmailAndPassword(
-          email: _emailController.text.trim(),
-          password: _passwordController.text,
-        );
-      } else {
-        await FirebaseAuth.instance.signInWithEmailAndPassword(
-          email: _emailController.text.trim(),
-          password: _passwordController.text,
-        );
-      }
-
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => ReadingScreen()),
-      );
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'weak-password') {
-        // TODO: handle weak password
-      } else if (e.code == 'email-already-in-use') {
-        // TODO: handle email already in use
-      } else if (e.code == 'user-not-found') {
-        // TODO: handle user not found
-      } else if (e.code == 'wrong-password') {
-        // TODO: handle wrong password
-      }
-    }
   }
 
   @override
